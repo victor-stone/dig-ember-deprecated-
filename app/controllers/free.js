50ce3a50d8fd41db356d631c030579812fe741cb
Victor Stone <victor.stone@gmail.com>
2015-09-04 23:18:20 -0700

    stragglers

---
+++ app/controllers/free.js
@@ -0,0 +1,10 @@
+/* globals Ember */
+import PageableController from './pageable';
+import { translationMacro as t } from "ember-i18n";
+
+export default PageableController.extend({
+    i18n: Ember.inject.service(),
+
+    title: t('free.title'),
+
+});
